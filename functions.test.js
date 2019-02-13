17093d4ea76ecb7c1773a020929f38ef2ba43b63
nguyenhu21 <nguyenhu21@up.edu>
2019-02-12 18:47:23 -0800

    Update functions.test.js

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,11 @@ const isNum = require('./functions.js');
 test('is 45 and 68 numbers', () => {
 	expect(isNum(45, 68)).toBe(true);
 });
+
+test('empty inputs', () => {
+	expect(isNum(" ", " ")).toBe(false);
+});
+
+test('is abc and def numbers', () => {
+	expect(isNum("abc", "def")).toBe(false);
+});
