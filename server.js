1ed7e1c991c1ccfe4abd8c911ba170449bc33a3a
PatrickMaloney <maloney20@up.edu>
2019-02-12 19:46:09 -0800

    Server now sends html as well as css

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    console.log("sent html to client");	
-    res.end();
-  });
+	if(req.url === '/index.html'){
+		fs.readFile('index.html', function(err, data) {
+    		res.writeHead(200, {'Content-Type': 'text/html'});
+    		res.write(data);
+    		console.log("sent html to client");	
+    		res.end();
+  		});
+	}
+	else if(req.url === '/style.css'){
+		fs.readFile('style.css', function(err, data) {
+			res.write(data);
+			console.log("sent css to client");
+			res.end();
+		});
+	}
 }).listen(8080);
 
 console.log("server running on port 8080");
