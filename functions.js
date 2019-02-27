fb5f90aed02b742c1d847074d8bf93ba244acf81
nguyenhu21 <nguyenhu21@up.edu>
2019-02-26 18:23:54 -0800

    update names of functions

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
   function: buttonPressed
   check to see if coordinate inputs are correct
 */
-function buttonPressed(firstNum, secondNum) {
+function checkCoor(firstNum, secondNum) {
 	//Check to see if any of the box is empty, return false if it is
 	if(firstNum === "" || secondNum === ""){
 		alert('one of the box is empty.');
@@ -30,4 +30,23 @@ function buttonPressed(firstNum, secondNum) {
 		return false;
 	}
 }
-module.exports = buttonPressed;
+
+/* Get x and y coordinates from user input */
+function getXY(xcoor, ycoor) {
+	//Check if inputs are valid
+	if(checkCoor(xcoor, ycoor) == false) {
+		throw 'coodinates inputs are incorrect';
+	}
+	//Array contains the x and y coordinates
+	var coor = [xcoor, ycoor];
+	//return the coordinates
+	return coor;
+}
+
+
+
+
+
+
+module.exports = getXY;
+module.exports = checkCoor;
